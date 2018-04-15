a5d23a47f2ccc6f26018fbc11d08d2805febe87e
JAMS <JAMS@DESKTOP-NCOS05C>
2018-04-15 18:44:03 -0500

    Clases de Clientes

---
+++ Bevisa_Cambio/src/negocio/Clientes.java
@@ -0,0 +1,208 @@
+
+package negocio;
+
+public class Clientes {
+    
+    private int  id;
+    private String nombre;
+    private String telefono;
+    private String celular;
+    private String correo;
+    private String delegacion;
+    private String colonia;
+    private String calle;
+    private String no_int;
+    private String no_ext;
+    private String rfc;
+    private String pago;
+    private String cuenta;
+    private String contacto;
+    private String estatus;
+    
+    private String nombref;
+    private String coloniaf;
+    private String callef;
+    private String no_intf;
+    private String no_extf;
+    private String delegacionf;
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDelegacion() {
+        return delegacion;
+    }
+
+    public void setDelegacion(String delegacion) {
+        this.delegacion = delegacion;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNo_int() {
+        return no_int;
+    }
+
+    public void setNo_int(String no_int) {
+        this.no_int = no_int;
+    }
+
+    public String getNo_ext() {
+        return no_ext;
+    }
+
+    public void setNo_ext(String no_ext) {
+        this.no_ext = no_ext;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getPago() {
+        return pago;
+    }
+
+    public void setPago(String pago) {
+        this.pago = pago;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getNombref() {
+        return nombref;
+    }
+
+    public void setNombref(String nombref) {
+        this.nombref = nombref;
+    }
+
+    public String getColoniaf() {
+        return coloniaf;
+    }
+
+    public void setColoniaf(String coloniaf) {
+        this.coloniaf = coloniaf;
+    }
+
+    public String getCallef() {
+        return callef;
+    }
+
+    public void setCallef(String callef) {
+        this.callef = callef;
+    }
+
+    public String getNo_intf() {
+        return no_intf;
+    }
+
+    public void setNo_intf(String no_intf) {
+        this.no_intf = no_intf;
+    }
+
+    public String getNo_extf() {
+        return no_extf;
+    }
+
+    public void setNo_extf(String no_extf) {
+        this.no_extf = no_extf;
+    }
+
+    public String getDelegacionf() {
+        return delegacionf;
+    }
+
+    public void setDelegacionf(String delegacionf) {
+        this.delegacionf = delegacionf;
+    }
+
+    public String getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(String vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    private String vendedor;
+    
+    
+}
